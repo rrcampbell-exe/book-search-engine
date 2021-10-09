6b0def4d75d0df0396c9f88c784097e743e2019f
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-10-09 10:27:03 -0500

    Write initial typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,42 @@
+// import the gql tagged template function
+const { gql } = require("apollo-server-express");
+
+// create our typeDefs
+const typeDefs = gql`
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookAuthor: [String], description: String, title: String, bookId: ID, image: String, link: String): User
+    removeBook(bookId: ID!): User
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    _id: ID
+    authors: [String]
+    description: String
+    title: Int
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+`;
+
+// export typeDefs
+module.exports = typeDefs;

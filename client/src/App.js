1d06f5887d67240e7c31a60ce960169247fe8bc0
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-10-14 06:32:23 -0500

    Add useMutation hook for saving of books

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import { setContext } from '@apollo/client/link/context'
 
 const httpLink = createHttpLink({
   uri: "/graphql",
--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
-
+import { SAVE_BOOK } from "../utils/mutations"
+import { useMutation } from "@apollo/client";
+import { GET_ME } from "../utils/queries"
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
@@ -13,6 +15,19 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+
+  // create mutation for book searches
+  const [saveBook] = useMutation(SAVE_BOOK, {
+    update(cache, { data: { saveBook } }) {
+
+      // update me object's cache, appending new book to the end of the array
+      const { me } = cache.readQuery({ query: GET_ME });
+      cache.writeQuery({
+        query: GET_ME,
+        data: { me: { ...me, books: [...me.savedBooks, saveBook] } },
+      });
+    },
+  });
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
